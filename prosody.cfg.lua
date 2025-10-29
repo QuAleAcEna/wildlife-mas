86a94de65d033ccff42fd764b586712b0d4177a9
Dinis Cunha <dinis@dcunha.pt>
2025-10-29 14:13:04 +0000

    update

--- prosody.cfg.lua
+++ prosody.cfg.lua
@@ -3,7 +3,7 @@ modules_enabled = {
   "roster"; "saslauth"; "tls"; "dialback"; "disco"; "carbons"; "pep"; "private";
   "blocklist"; "vcard4"; "vcard_legacy"; "version"; "uptime"; "time"; "ping"; "bosh";
 }
-allow_registration = false
+allow_registration = true
 c2s_require_encryption = false
 s2s_require_encryption = false
 allow_unencrypted_plain_auth = true
